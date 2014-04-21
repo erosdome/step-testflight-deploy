e65739c01959c110d234443d17b9291f2b63477d
Barnabás Birmacher <birmacher@gmail.com>
2014-04-21 16:42:46 +0200

    test flight team and api token fix

--- step.sh
+++ step.sh
@@ -47,8 +47,8 @@ if [ -f "$TF_DSYM" ]; then
   curl http://testflightapp.com/api/builds.json \
     -F file=@$TF_IPA \
     -F dsym=@$TF_DSYM \
-    -F team_token='"$TF_TEAM_TOKEN"' \
-    -F api_token='"$TF_API_TOKEN"' \
+    -F team_token=$TF_TEAM_TOKEN \
+    -F api_token=$TF_API_TOKEN \
     -F notes='"$TF_NOTES"' \
     -F distribution_lists='"$TESTFLIGHT_DISTRIBUTION_LIST"' \
     -F notify=$TF_NOTIFY \
@@ -56,8 +56,8 @@ if [ -f "$TF_DSYM" ]; then
 else 
   curl http://testflightapp.com/api/builds.json \
     -F file=@$TF_IPA \
-    -F team_token='"$TF_TEAM_TOKEN"' \
-    -F api_token='"$TF_API_TOKEN"' \
+    -F team_token=$TF_TEAM_TOKEN \
+    -F api_token=$TF_API_TOKEN \
     -F notes='"$TF_NOTES"' \
     -F distribution_lists='"$TESTFLIGHT_DISTRIBUTION_LIST"' \
     -F notify=$TF_NOTIFY \
