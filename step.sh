563b1b3332fc0d3c3016a27cb8b53a4b6810ef18
Viktor Benei <viktor.benei@gmail.com>
2014-07-15 20:51:34 +0200

    step.sh - indentation converted to tabs

--- step.sh
+++ step.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 function echoStatusFailed {
-  echo "export TESTFLIGHT_DEPLOY_STATUS=\"failed\"" >> ~/.bash_profile
-  echo
-  echo "TESTFLIGHT_DEPLOY_STATUS: \"failed\""
-  echo " --------------"
+	echo "export TESTFLIGHT_DEPLOY_STATUS=\"failed\"" >> ~/.bash_profile
+	echo
+	echo "TESTFLIGHT_DEPLOY_STATUS: \"failed\""
+	echo " --------------"
 }
 
 # default values
 
 if [[ $TESTFLIGHT_NOTES ]]; then
-  notes=$TESTFLIGHT_NOTES
+	notes=$TESTFLIGHT_NOTES
 else
 	notes="Automatic build with Bitrise."
 fi
@@ -18,13 +18,13 @@ fi
 notif_lower=`echo $TESTFLIGHT_NOTIFY | tr '[:upper:]' '[:lower:]'`
 notify=""
 if [[ $TESTFLIGHT_NOTIFY ]] && [[ "$notif_lower" = "true" ]]; then
-  notify="true"
+	notify="true"
 fi
 
 replace_lower=`echo $TESTFLIGHT_REPLACE | tr '[:upper:]' '[:lower:]'`
 replace=""
 if [[ $TESTFLIGHT_REPLACE ]] && [[ "$replace_lower" = "true" ]]; then
-  replace="true"
+	replace="true"
 fi
 
 echo "BITRISE_IPA_PATH: $BITRISE_IPA_PATH"
@@ -40,40 +40,40 @@ echo "TESTFLIGHT_DISTRIBUTION_LIST: $TESTFLIGHT_DISTRIBUTION_LIST"
 
 # IPA
 if [[ ! -f "$BITRISE_IPA_PATH" ]]; then
-  echo
-  echo "No IPA found to deploy. Terminating..."
-  echo
-  echoStatusFailed
-  exit 1
+	echo
+	echo "No IPA found to deploy. Terminating..."
+	echo
+	echoStatusFailed
+	exit 1
 fi
 
 # dSYM if provided
 if [[ $BITRISE_DSYM_PATH ]]; then
 	if [[ ! -f "$BITRISE_DSYM_PATH" ]]; then
-    echo
-    echo "No DSYM found to deploy. Terminating..."
-    echo
-    echoStatusFailed
-    exit 1
-  fi
+		echo
+		echo "No DSYM found to deploy. Terminating..."
+		echo
+		echoStatusFailed
+		exit 1
+	fi
 fi
 
 # API token
 if [[ ! $TESTFLIGHT_API_TOKEN ]]; then
-  echo
-  echo "No API token provided as environment variable. Terminating..."
-  echo
-  echoStatusFailed
-  exit 1
+	echo
+	echo "No API token provided as environment variable. Terminating..."
+	echo
+	echoStatusFailed
+	exit 1
 fi
 
 # Team token
 if [[ ! $TESTFLIGHT_TEAM_TOKEN ]]; then
-  echo
-  echo "No Team token provided as environment variable. Terminating..."
-  echo
-  echoStatusFailed
-  exit 1
+	echo
+	echo "No Team token provided as environment variable. Terminating..."
+	echo
+	echoStatusFailed
+	exit 1
 fi
 
 json=$(/usr/bin/curl http://testflightapp.com/api/builds.json \
@@ -94,10 +94,10 @@ echo " --------------\n"
 # ERROR CHECK - if not json result, then something bad happened
 
 if [[ ! $json =~ }.* ]]; then
-  echo " --FAILED--"
-  echo "$json"
-  echoStatusFailed
-  exit 1
+	echo " --FAILED--"
+	echo "$json"
+	echoStatusFailed
+	exit 1
 fi
 
 # everything is OK
@@ -106,15 +106,15 @@ echo "export TESTFLIGHT_DEPLOY_STATUS=\"success\"" >> ~/.bash_profile
 
 # install url
 install_url=$(ruby ./steps-utils-jsonval/parse_json.rb \
-  --json-string="$json" \
-  --prop=install_url)
+	--json-string="$json" \
+	--prop=install_url)
 
 echo "export TESTFLIGHT_DEPLOY_INSTALL_URL=\"$install_url\"" >> ~/.bash_profile
 
 # config url
 config_url=`ruby ./steps-utils-jsonval/parse_json.rb \
-  --json-string="$json" \
-  --prop=config_url`
+	--json-string="$json" \
+	--prop=config_url`
 echo "export TESTFLIGHT_DEPLOY_CONFIG_URL=\"$config_url\"" >> ~/.bash_profile
 
 # final results
